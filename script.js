fb2273f82f8347a2521e2ef4e2a26d361b75a8c1
itsgreatness <92487172+itsgreatness@users.noreply.github.com>
2022-09-21 18:47:42 -0500

    e

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ window.setInterval(()=>{
     i++;
     if (i % 2 == 0) {
         el.textContent = "SMASH";
+        document.title = "SMASH";
     } else {
         el.textContent = "PASS";
+        document.title = "PASS";
     }
 },3000);
