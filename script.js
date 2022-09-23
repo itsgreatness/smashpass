1b3528374a386667d56249fe38618c3fbec97a82
itsgreatness <92487172+itsgreatness@users.noreply.github.com>
2022-09-23 18:30:28 -0500

    |  | oo

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
+function addText(ot, ad, ml) {
+    return (ot + ad).trim().split(" ").splice((ot + ad).trim().split(" ").length-ml, (ot + ad).trim().split(" ").length).join(" ").trim();
+}
+function regulateRange(i, r) {
+    return i % r;
+}
 const el = document.getElementById("smashpass");
 let i = 0;
+let word = "";
 window.setInterval(()=>{
-    i++;
-    if (i % 2 == 0) {
-        el.textContent = "SMASH";
-        document.title = "SMASH";
-    } else {
-        el.textContent = "PASS";
-        document.title = "PASS";
+    i = regulateRange(i, 3);
+    if (i == 0) {
+        word = addText(word, " SMASH", 2);
+    } else 
+    if (i == 1) {
+        word = addText(word, " OR", 2);
+    } else
+    if (i == 2) {
+        word = addText(word, " PASS", 2);
     }
+    el.textContent = word;
+    document.title = word;
+    i++;
 },3000);
