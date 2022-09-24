dd3116db7f5bca2a5b6dce9498bda4b7cf876428
itsgreatness <92487172+itsgreatness@users.noreply.github.com>
2022-09-24 12:25:16 -0500

    bah humbug

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 function regulateLength(s, ml) {
     return s.substring(s.length-ml,s.length);
 }
+/*
+    remember when i implemented this function ^
+    well theres a string length limit and the memory consumption
+    so will have to code it so that it is *possible* to optimize
+    the "word" var itself instead of just the visible output
+    
+    i bet youre thinking its very easy, future me
+    trust me its not when the "word" var maintains the same length 
+    it will keep adding the first and *only* the first letter 
+    from "MAX_WORD" i hope you understand why or else youre dumber 
+    than i thought
+
+    OK, pushing this change now
+*/
 function tick() {
     word += MAX_WORD[word.length%MAX_WORD.length];
     document.title = regulateLength(word, MAX_WORD.length);
